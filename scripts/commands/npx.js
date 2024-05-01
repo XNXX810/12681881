2e9d87d5269674ea0af3cc6be46205241f10fbe2
Mahabub5103 <168609562+Mahabub5103@users.noreply.github.com>
2024-05-01 09:27:49 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "😒",
+	name: "🤰🏽",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
 	credits: "nayan", 
 	description: "Fun",
 	category: "no prefix",
-	usages: "😒",
+	usages: "🤰🏽",
         cooldowns: 5, 
 },
 
@@ -16,13 +16,13 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
+	if (body.indexOf("🤰🏽")==0 || body.indexOf("🤰🏼")==0 || body.indexOf("🤰🏻")==0 || body.indexOf("🤰")==0) {
 		var msg = {
-				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
-				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
+				body: "এইটা তুমি কি করলা বাবু 🥺🤰",
+				attachment: fs.createReadStream(__dirname + `/Nayan/scripts_commands_cache_pregnant.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😩", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
